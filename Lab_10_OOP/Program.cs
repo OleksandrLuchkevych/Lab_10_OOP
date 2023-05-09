ff995ea33deae0121a6c300e89387b78e0ecc639
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2023-05-09 09:01:36 +0300

    Add project files.

---
+++ Lab_10_OOP/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.Serialization.Json;
+
+namespace Lab_10_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point Zero = new Point(0, 0);
+
+            List<Triangle> triangleList = new List<Triangle>
+            {
+                new Triangle(new Point(0, 0), new Point(2, 4), new Point(4, 0)),
+                new Triangle(new Point(9, 0), new Point(0, 4), new Point(2, 0)),
+                new Triangle(new Point(4, 7), new Point(2, 6), new Point(1, 0)),
+
+            };
+            foreach (Triangle triangle in triangleList)
+            {
+                triangle.Print();
+                Console.WriteLine();
+            }
+
+            Triangle closestTriangle = triangleList[0];
+            double closestDistance = closestTriangle.MinimalDistance(Zero);
+
+            foreach (Triangle triangle in triangleList)
+            {
+                double distance = triangle.MinimalDistance(Zero);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestTriangle = triangle;
+                }
+            }
+
+            Console.WriteLine("Triangle closest to (0, 0):");
+            closestTriangle.Print();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Stream file = new FileStream("Triangle.json", FileMode.Create);
+            DataContractJsonSerializer ser = new
+                                           DataContractJsonSerializer(typeof(List<Triangle>));
+            ser.WriteObject(file, triangleList);
+
+
+
+            Console.WriteLine("After serialization: ");
+            file.Position = 0;
+            List<Triangle> triangleList1 = (List<Triangle>)ser.ReadObject(file);
+            foreach (Triangle triangles in triangleList1)
+            {
+                triangles.Print();
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Lab_10_OOP/Triangle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+namespace Lab_10_OOP
+{
+    [DataContract]
+    public struct Point
+    {
+        [DataMember]
+        public int X;
+        [DataMember]
+        public int Y;
+        public Point(int x, int y)
+        {
+            X = x; Y = y;
+        }
+        public override string ToString()
+        {
+            return $"Point ({X};{Y})";
+        }
+    }
+    [DataContract]
+    public class Triangle
+    {
+        [DataMember]
+        private Point _vertex1, _vertex2, _vertex3;
+
+        public Triangle(Point vertex1, Point vertex2, Point vertex3)
+        {
+            _vertex1 = vertex1;
+            _vertex2 = vertex2;
+            _vertex3 = vertex3;
+        }
+
+        public double Distance(Point first, Point second)
+        {
+            return Math.Sqrt(Math.Pow((first.X - second.X), 2) + Math.Pow((first.Y - second.Y), 2));
+        }
+        public double Perimeter()
+        {
+            double distance1 = Distance(_vertex1, _vertex2);
+            double distance2 = Distance(_vertex1, _vertex3);
+            double distance3 = Distance(_vertex2, _vertex3);
+
+            return distance1 + distance2 + distance3;
+        }
+        public double Square()
+        {
+            double distance1 = Distance(_vertex1, _vertex2);
+            double distance2 = Distance(_vertex1, _vertex3);
+            double distance3 = Distance(_vertex2, _vertex3);
+            double HalfPerimeter = Perimeter() / 2;
+
+            return Math.Sqrt(HalfPerimeter * (HalfPerimeter - distance1) * (HalfPerimeter - distance2) * (HalfPerimeter - distance3));
+        }
+
+        public double MinimalDistance(Point zero)
+        {
+            double distance1 = Distance(zero, _vertex1);
+            double distance2 = Distance(zero, _vertex2);
+            double distance3 = Distance(zero, _vertex3);
+
+            return Math.Min(distance1, Math.Min(distance2, distance3));
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Vertices of the triangle: {_vertex1}, {_vertex2}, {_vertex3}, Perimetr is {Perimeter()} and Square is {Square()} ");
+        }
+
+    }
+}
